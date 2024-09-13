3207040597d9455451d34c53174c46f0692da7d1
Abhinav Kumar Singh <abhinavkkrsingh@gmail.com>
2024-09-13 06:44:55 -0700

    Update BasicProgramming.dart

--- BasicProgramming.dart
+++ BasicProgramming.dart
@@ -1,3 +1,5 @@
+Swapping in programming is the act of exchanging the values of two variables. This allows for the rearrangement of data within a program's memory space, which can facilitate algorithms and operations
+
 //Swapping a number........
 void main() {
   int a = 29;
